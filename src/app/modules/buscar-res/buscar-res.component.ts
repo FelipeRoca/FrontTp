6be0aebb00cd45c2a947a9f924b0adf12a9b6d76
FelipeRoca <feliroca01@gmail.com>
2023-12-12 15:17:45 -0300

    Inserto mapa en la seccion de busq de ciudades

--- src/app/modules/buscar-res/buscar-res.component.ts
+++ src/app/modules/buscar-res/buscar-res.component.ts
@@ -1,56 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+
+
+import { Component, OnInit } from '@angular/core';
 import { ResServiceService } from '../services/res-service.service';
 import { BuscarResService } from '../services/buscar-res.service';
 import { MenuItem } from 'primeng/api';
-
-
-// @Component({
-//   selector: 'app-buscar-res',
-//   templateUrl: './buscar-res.component.html',
-//   styleUrls: ['./buscar-res.component.css']
-// })
-// export class BuscarResComponent implements OnInit{
-//   ngOnInit(): void {
-//     this.resServiceService.getReviews().subscribe(reviews => {
-//       this.reviews = reviews
-//       console.log(this.reviews)
-//     })
-//   }
-
-//   private resServiceService = inject(ResServiceService)
-//   private buscarResService = inject(BuscarResService)
-
-
-//   public reviews:any=[]
-
-//    buscarReview(cityName: any): void {
-//      console.log(cityName)
-//      this.buscarResService.getReviewsByCityName(cityName.value).subscribe(reviews => {
-//        this.reviews = reviews
-//        console.log(this.reviews) 
-
-//     })
-//  }
-
-
-
-
-
-
-
-// mostrarInput(valor: any){
-//   this.buscarResService.getReviewsByCityName(valor.value).subscribe(reviews => {
-//     this.reviews = reviews
-//     console.log(this.reviews) 
-
-//   })
-
-// }
-
-// }
-// ... importaciones ...
-
-// ... importaciones ...
+import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-buscar-res',
@@ -58,43 +13,85 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./buscar-res.component.css']
 })
 export class BuscarResComponent implements OnInit {
-  reviews: any[] = [];  // Define reviews como un array de tipo any
+  reviews: any[] = [];
   items: MenuItem[] | undefined;
+  map: L.Map | undefined;
+
   constructor(
     private resServiceService: ResServiceService,
-    private buscarResService: BuscarResService
+    private buscarResService: BuscarResService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
     this.items = [
       {
-          label: 'Inicio',
-          routerLink: ['/inicio'],
-          icon: 'pi pi-fw pi-file',
-          items: [] 
+        label: 'Inicio',
+        routerLink: ['/inicio'],
+        icon: 'pi pi-fw pi-file',
+        items: [] 
       },
       {
-          label: 'Iniciar Sesion',
-          routerLink: ['/iniciar-sesion'],
-          icon: 'pi pi-fw pi-user',
-          items: []
+        label: 'Iniciar Sesion',
+        routerLink: ['/iniciar-sesion'],
+        icon: 'pi pi-fw pi-user',
+        items: []
       },
       {
-          label: 'Registrarse',
-          routerLink: ['/registrarse'],
-          icon: 'pi pi-fw pi-calendar',
-          items: []
+        label: 'Registrarse',
+        routerLink: ['/registrarse'],
+        icon: 'pi pi-fw pi-calendar',
+        items: []
       },
-  ];
+    ];
+
     this.resServiceService.getReviews().subscribe((reviews: any) => {
       this.reviews = Array.isArray(reviews) ? reviews : [reviews];
       console.log(this.reviews);
+
+
+      this.inicializarMapa();
+      this.geocodeYMostrarLugaresEnMapa();
     });
   }
-  
 
-  buscarReview(cityName: any): void {
-    console.log(cityName);
+  inicializarMapa() {
+    this.map = L.map('map').setView([0, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  geocodeYMostrarLugaresEnMapa() {
+    if (!this.map) {
+      console.error('El mapa no está inicializado.');
+      return;
+    }
+
+    this.reviews.forEach(review => {
+      const direccion = `${review.City.name}, ${review.City.Country.name}`;
+      this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
+        const location = result[0];
+        if (location && location.lat && location.lon) {
+          const marker = L.marker([location.lat, location.lon]).addTo(this.map!);
+          marker.bindPopup(direccion).openPopup();
+        } else {
+          console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
+        }
+      });
+    });
+  }
+
+  geocodeDireccionNominatim(direccion: string) {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
+    return this.http.get(url);
+  }
+
+  buscarReview(cityName: any, countryName: any): void {
+    console.log(cityName, countryName);
+    const direccion = `${cityName.value}, ${countryName.value}`;
+    
     this.buscarResService.getReviewsByCityName(cityName.value).subscribe(reviews => {
       if (Array.isArray(reviews)) {
         this.reviews = reviews;
@@ -102,6 +99,15 @@ export class BuscarResComponent implements OnInit {
         this.reviews = [reviews];
       }
       console.log(this.reviews);
+
+      this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
+        const location = result[0];
+        if (location && location.lat && location.lon && this.map) {
+          this.map.setView([location.lat, location.lon], 10);
+        } else {
+          console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
+        }
+      });
     });
   }
 
@@ -113,9 +119,31 @@ export class BuscarResComponent implements OnInit {
         this.reviews = [reviews];
       }
       console.log(this.reviews);
+
+      const ciudad = this.reviews[0].City;
+      const direccion = `${ciudad.name}, ${ciudad.Country.name}`;
+      this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
+        const location = result[0];
+        if (location && location.lat && location.lon && this.map) {
+          this.map.setView([location.lat, location.lon], 10);
+        } else {
+          console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
+        }
+      });
     });
   }
+  restablecerCiudades(): void {
+    this.resServiceService.getReviews().subscribe((reviews: any) => {
+      this.reviews = Array.isArray(reviews) ? reviews : [reviews];
+      console.log(this.reviews);
+        if (this.map) {
+          this.map.remove();
+        }
+      
+        this.inicializarMapa();
+        this.geocodeYMostrarLugaresEnMapa();
+      }
+    );
+  }
+
 }
-
-
-
