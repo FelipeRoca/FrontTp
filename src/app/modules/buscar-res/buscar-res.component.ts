9536d1f483ceb621339d5895f890719f369046a2
FelipeRoca <feliroca01@gmail.com>
2025-02-07 19:21:09 -0300

    Seccion de videos de youtube

--- src/app/modules/buscar-res/buscar-res.component.ts
+++ src/app/modules/buscar-res/buscar-res.component.ts
@@ -1,192 +1,3 @@
-
-// import { Component, OnInit } from '@angular/core';
-// import { ResServiceService } from '../services/res-service.service';
-// import { BuscarResService } from '../services/buscar-res.service';
-// import { MenuItem } from 'primeng/api';
-// import { HttpClient } from '@angular/common/http';
-// import * as L from 'leaflet';
-
-// @Component({
-//   selector: 'app-buscar-res',
-//   templateUrl: './buscar-res.component.html',
-//   styleUrls: ['./buscar-res.component.css']
-// })
-// export class BuscarResComponent implements OnInit {
-//   reviews: any[] = [];
-//   items: MenuItem[] | undefined;
-//   map: L.Map | undefined;
-//   resButton: boolean = false;
-//   showTripAdvisor: boolean = false;
-//   selectedCity: string = '';
-//   tripAdvisorSearchUrl: string = '';
-
-//   weather: string = '';
-//   temperature: number | null = null;
-//   humidity: number | null = null;
-
-//   constructor(
-//     private resServiceService: ResServiceService,
-//     private buscarResService: BuscarResService,
-//     private http: HttpClient
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.items = [
-//       {
-//         label: 'Inicio',
-//         routerLink: ['/inicio'],
-//         icon: 'pi pi-fw pi-file',
-//         items: []
-//       },
-//       {
-//         label: 'Iniciar Sesion',
-//         routerLink: ['/iniciar-sesion'],
-//         icon: 'pi pi-fw pi-user',
-//         items: []
-//       },
-//       {
-//         label: 'Registrarse',
-//         routerLink: ['/registrarse'],
-//         icon: 'pi pi-fw pi-calendar',
-//         items: []
-//       },
-//     ];
-
-    
-
-//     this.resServiceService.getReviews().subscribe((reviews: any) => {
-//       this.reviews = Array.isArray(reviews) ? reviews : [reviews];
-//       console.log(this.reviews);
-
-//       this.inicializarMapa();
-//       this.geocodeYMostrarLugaresEnMapa();
-//     });
-//   }
-
-//   getWeather(cityName: string): void {
-//     const apiKey = '783c00ab61d5af6f40597c14d87c35dd';
-//     const url = `https://api.weatherstack.com/current?access_key=${apiKey}&query=${encodeURIComponent(cityName)}`;
-
-
-//     this.http.get(url).subscribe((data: any) => {
-//       if (data && data.current) {
-//         const temp = data.current.temperature;
-//         const weatherDesc = data.current.weather_descriptions[0];
-//         const humidity = data.current.humidity;
-//         this.weather = weatherDesc;
-//         this.temperature = temp;
-//         this.humidity = humidity;
-//       } else {
-//         console.error('No se pudo obtener el clima para la ciudad');
-//       }
-//     });
-//   }
-
-//   inicializarMapa() {
-//     this.map = L.map('map').setView([0, 0], 2);
-
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '© OpenStreetMap contributors'
-//     }).addTo(this.map);
-//   }
-
-//   geocodeYMostrarLugaresEnMapa() {
-//     if (!this.map) {
-//       console.error('El mapa no está inicializado.');
-//       return;
-//     }
-
-//     this.reviews.forEach(review => {
-//       const direccion = `${review.City.name}, ${review.City.Country.name}`;
-//       this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
-//         const location = result[0];
-//         if (location && location.lat && location.lon) {
-//           const marker = L.marker([location.lat, location.lon]).addTo(this.map!);
-//           marker.bindPopup(direccion).openPopup();
-//         } else {
-//           console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
-//         }
-//       });
-//     });
-//   }
-
-//   geocodeDireccionNominatim(direccion: string) {
-//     const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
-//     return this.http.get(url);
-//   }
-
-//   buscarReview(cityName: any, countryName: any): void {
-//     console.log(cityName, countryName);
-//     const direccion = `${cityName.value}, ${countryName.value}`;
-//     this.resButton = true;
-    
-//     this.buscarResService.getReviewsByCityName(cityName.value).subscribe(reviews => {
-//       if (Array.isArray(reviews)) {
-//         this.reviews = reviews;
-//       } else {
-//         this.reviews = [reviews];
-//       }
-//       console.log(this.reviews);
-
-//       this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
-//         const location = result[0];
-//         if (location && location.lat && location.lon && this.map) {
-//           this.map.setView([location.lat, location.lon], 10);
-//         } else {
-//           console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
-//         }
-//       });
-//     });
-//   }
-
-//   mostrarInput(valor: any): void {
-//     this.selectedCity = valor.value;
-//     this.buscarResService.getReviewsByCityName(valor.value).subscribe(reviews => {
-//       if (Array.isArray(reviews)) {
-//         this.reviews = reviews;
-//       } else {
-//         this.reviews = [reviews];
-//       }
-//       console.log(this.reviews);
-
-//       const ciudad = this.reviews[0].City;
-//       const direccion = `${ciudad.name}, ${ciudad.Country.name}`;
-//       this.geocodeDireccionNominatim(direccion).subscribe((result: any) => {
-//         const location = result[0];
-//         if (location && location.lat && location.lon && this.map) {
-//           this.map.setView([location.lat, location.lon], 10);
-//         } else {
-//           console.warn(`No se encontraron coordenadas para la dirección: ${direccion}`);
-//         }
-//       });
-
-//       this.getTripAdvisorHotels(this.selectedCity);
-//       this.getWeather(this.selectedCity);
-//     });
-//   }
-
-//   getTripAdvisorHotels(city: string) {
-//     this.tripAdvisorSearchUrl = `https://www.tripadvisor.com/Search?q=hotels+in+${encodeURIComponent(city)}`;
-//     this.showTripAdvisor = true;
-//   }
-
-//   restablecerCiudades(): void {
-//     this.resServiceService.getReviews().subscribe((reviews: any) => {
-//       this.reviews = Array.isArray(reviews) ? reviews : [reviews];
-//       console.log(this.reviews);
-//       if (this.map) {
-//         this.map.remove();
-//       }
-      
-//       this.inicializarMapa();
-//       this.geocodeYMostrarLugaresEnMapa();
-//       this.showTripAdvisor = false;
-//       this.tripAdvisorSearchUrl = '';
-//     });
-//   }
-// }
-
-
 import { Component, OnInit } from '@angular/core';
 import { ResServiceService } from '../services/res-service.service';
 import { BuscarResService } from '../services/buscar-res.service';
@@ -194,6 +5,8 @@ import { WeatherService } from '../services/weather.service';  // Importar el se
 import { GeocodeService } from '../services/geocode.service';  // Importar el servicio de geocodificación
 import { MenuItem } from 'primeng/api';
 import * as L from 'leaflet';
+import { YoutubeService } from '../../services/youtube.service';
+
 
 @Component({
   selector: 'app-buscar-res',
@@ -208,6 +21,7 @@ export class BuscarResComponent implements OnInit {
   showTripAdvisor: boolean = false;
   selectedCity: string = '';
   tripAdvisorSearchUrl: string = '';
+  videos: any[] = [];
 
   weather: string = '';
   temperature: number | null = null;
@@ -217,7 +31,8 @@ export class BuscarResComponent implements OnInit {
     private resServiceService: ResServiceService,
     private buscarResService: BuscarResService,
     private weatherService: WeatherService,  // Inyectar WeatherService
-    private geocodeService: GeocodeService,  // Inyectar GeocodeService
+    private geocodeService: GeocodeService, // Inyectar GeocodeService
+    private youtubeService: YoutubeService 
   ) {}
 
   ngOnInit(): void {
@@ -248,6 +63,7 @@ export class BuscarResComponent implements OnInit {
 
       this.inicializarMapa();
       this.geocodeYMostrarLugaresEnMapa();
+      this.getVideos('');
     });
   }
 
@@ -343,6 +159,7 @@ export class BuscarResComponent implements OnInit {
 
       this.getTripAdvisorHotels(this.selectedCity);
       this.getWeather(this.selectedCity);
+      this.getVideos(this.selectedCity);
     });
   }
 
@@ -350,6 +167,39 @@ export class BuscarResComponent implements OnInit {
   getTripAdvisorHotels(city: string) {
     this.tripAdvisorSearchUrl = `https://www.tripadvisor.com/Search?q=hotels+in+${encodeURIComponent(city)}`;
     this.showTripAdvisor = true;
+  }
+
+
+  getVideos(city: string): void {
+    if (city.trim() === '') {
+      console.log('No hay búsqueda, cargando videos por defecto...');
+      this.youtubeService.buscarVideosPorDefecto().subscribe(response => {
+        this.procesarRespuesta(response);
+      }, error => {
+        console.error('Error al obtener videos por defecto de YouTube', error);
+      });
+    } else {
+      this.youtubeService.buscarVideos(city).subscribe(response => {
+        this.procesarRespuesta(response);
+      }, error => {
+        console.error('Error al obtener videos de YouTube', error);
+      });
+    }
+  }
+  
+  // ✅ Función para procesar la respuesta de la API y extraer los videos
+  private procesarRespuesta(response: any): void {
+    console.log('Respuesta de la API de YouTube:', response);
+    if (response.items && response.items.length > 0) {
+      this.videos = response.items.map((item: any) => ({
+        title: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.medium.url,
+        videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`
+      }));
+    } else {
+      console.warn('No se encontraron videos.');
+      this.videos = [];
+    }
   }
 
   // Restablecer ciudades
---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private apiKey = 'AIzaSyCskW1CM8-kSlACeFGY0s4F3n-TgPfqrPA';  // Reemplázala con tu clave real
+  private apiUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+  constructor(private http: HttpClient) {}
+
+  // 🔍 Busca videos según una ciudad ingresada
+  buscarVideos(ciudad: string): Observable<any> {
+    const url = `${this.apiUrl}?part=snippet&type=video&q=${encodeURIComponent(ciudad)} turismo&maxResults=3&key=${this.apiKey}`;
+    return this.http.get<any>(url);
+  }
+
+  // 🌍 Busca videos de turismo al azar si no hay búsqueda
+  buscarVideosPorDefecto(): Observable<any> {
+    const temas = ['viajes', 'turismo', 'destinos increíbles', 'playas hermosas', 'ciudades hermosas'];
+    const temaAleatorio = temas[Math.floor(Math.random() * temas.length)];
+    const url = `${this.apiUrl}?part=snippet&type=video&q=${encodeURIComponent(temaAleatorio)}&maxResults=3&key=${this.apiKey}`;
+    console.log('Cargando videos por defecto:', url);
+    return this.http.get<any>(url);
+  }
+}
